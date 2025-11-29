9e6fef0ee6fdebc4a484e144a6bd58cdc5300b9e
1TONG1 <adamtong100@gmail.com>
2025-11-29 17:23:44 -0500

    final commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,63 @@
 #include "main.h"
 #include "lemlib/api.hpp"
+#include "pros/adi.hpp"
 
-pros::MotorGroup left_motors({-1, -2, -3}, pros::MotorGearset::red);
-pros::MotorGroup right_motors({4, 5, 6});
+pros::MotorGroup left_motors({-1, -2, -3}, pros::MotorGearset::blue);
+pros::MotorGroup right_motors({4, 5, 6}, pros::MotorGearset::blue);
+
+lemlib::Drivetrain drivetrain(&left_motors, 
+								&right_motors, 
+								11.75, 
+								Omniwheel::NEW_325, 
+								450, 
+								2);
+
+pros::Imu imu (20);
+pros::Rotation horizontal_tracker (-9);
+pros::Rotation vertical_tracker (-10);
+
+lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_tracker, Omniwheel::NEW_325, -2.5);
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_tracker, Omniwheel::NEW_325, 0);
+
+lemlib::OdomSensors sensors(&vertical_tracking_wheel, 
+							nullptr, 
+							&horizontal_tracking_wheel, 
+							nullptr, 
+							&imu);
+
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              0, // anti windup
+                                              0, // small error range, in degrees
+                                              0, // small error range timeout, in milliseconds
+                                              0, // large error range, in degrees
+                                              0, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+lemlib::Chassis chassis(drivetrain, 
+						lateral_controller, 
+						angular_controller, 
+						sensors);
+pros::adi::Pneumatics trapdoor('C', false);
+pros::adi::Pneumatics match_load('G', false);
+pros::adi::Pneumatics wing_descore('E', false);
+pros::Motor intake1(7, pros::MotorGearset::blue);
+pros::Motor intake2(8, pros::MotorGearset::blue);
+
 
 // --- HELPER FUNCTIONS --- //
 
@@ -15,7 +70,7 @@ pros::MotorGroup right_motors({4, 5, 6});
 // void wing_descore_move(bool up_down){
 //         wing_descore.set_value(up_down);
 // }
-// void intake_stg1_move(bool intake){
+// void intake1_move(bool intake){
 //         if (intake){
 //                 stage1_intake_motor.move_velocity(600);
 //         }
@@ -23,7 +78,7 @@ pros::MotorGroup right_motors({4, 5, 6});
 //                 stage1_intake_motor.move_velocity(-600);
 //         }
 // }
-// void intake_stg2_move(bool cycle){
+// void intake2_move(bool cycle){
 //         if (cycle){
 //                 stage2_intake_motor.move_velocity(600);
 //         }
@@ -32,25 +87,19 @@ pros::MotorGroup right_motors({4, 5, 6});
 //         }
 // }
 
-// void intake_stg1_stop(){
+// void intake1_stop(){
 //         stage1_intake_motor.move_velocity(0);
 // }
-// void intake_stg2_stop(){
+// void intake2_stop(){
 //         stage2_intake_motor.move_velocity(0);
 // }
-// void intake_stg1_move_velocity_percent(int velocity){
+// void intake1_move_velocity_percent(int velocity){
 //         stage1_intake_motor.move_velocity(velocity*6);
 // }
-// void intake_stg2_move_velocity_percent(int velocity){
+// void intake2_move_velocity_percent(int velocity){
 //         stage2_intake_motor.move_velocity(velocity*6);
 // }
 
-// int loop_delay_ms = 20;
-// bool intake_stg3_O_F = false;
-// bool trapdoor_O_F = false;
-// bool loader_O_F = false;
-// bool intake_stg2_O_F = false;
-// bool wing_descore_O_F = false;
 
 // 
 
@@ -62,15 +111,15 @@ pros::MotorGroup right_motors({4, 5, 6});
  */
 void initialize() {
         lcd::initialize();
-        // chassis.calibrate(); // calibrate sensors  // Change later
+        chassis.calibrate(); // calibrate sensors
     
         // print position to brain screen
         pros::Task screen_task([&]() {
                 while (true) {
                         // print robot location to the brain screen
-                        // pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
-                        // pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
-                        // pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+                        pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+                        pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+                        pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
                         // delay to save resources
                         pros::delay(20);
                 }
@@ -198,6 +247,5 @@ G - match load
 E - wing
 C - Middle goal
 
-Maybe port A as horizontal tracker
 9, 10, horizontal and vertical tracking
 */
